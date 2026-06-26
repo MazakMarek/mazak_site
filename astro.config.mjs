9d964968b3c4fca7f1807e2c0de7ea27292ac663
MazakMarek <149437498+MazakMarek@users.noreply.github.com>
2026-06-26 09:45:19 +0200

    Change of design layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// export default defineConfig({
+//   site: 'https://MazakMarek.github.io',
+//   base: '/mazak_site/',
+// });
 export default defineConfig({
-  site: 'https://MazakMarek.github.io',
   base: '/mazak_site/',
+  trailingSlash: 'always',
 });
